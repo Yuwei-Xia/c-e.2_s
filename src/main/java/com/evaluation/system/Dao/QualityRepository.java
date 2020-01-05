0dbb4f3f7d050efe7eadda018e315dde01de546b
sky-cloud-debug <1115178050@qq.com>
2020-01-05 17:15:32 +0800

    2020.1.5 宋

---
+++ src/main/java/com/evaluation/system/Dao/QualityRepository.java
@@ -0,0 +1,8 @@
+package com.evaluation.system.Dao;
+
+import com.evaluation.system.domain.quality;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QualityRepository extends JpaRepository<quality,Integer> {
+
+}
