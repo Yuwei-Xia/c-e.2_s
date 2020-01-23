480fa7ed812e49e550b92e7c8dda0e356693a164
Yuwei-Xia <2328998404@qq.com>
2020-01-23 12:09:45 +0800

    IDEA整合GitHub测试

--- src/main/java/com/evaluation/system/Dao/QualityRepository.java
+++ src/main/java/com/evaluation/system/Dao/QualityRepository.java
@@ -4,5 +4,7 @@ import com.evaluation.system.domain.quality;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QualityRepository extends JpaRepository<quality,Integer> {
+    public quality findBywisdom(double asd);
+
 
 }
